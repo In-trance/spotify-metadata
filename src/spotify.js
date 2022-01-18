59c9dbb2e84d2e8661fd57f971a123580ce1c371
In-trance <influx00@tutanota.com>
2022-01-18 17:01:56 +0000

    iitial commit

--- src/spotify.js
+++ src/spotify.js
@@ -1,8 +1,8 @@
 // https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 // Replace with your app's client ID, redirect URI and desired scopes
-const clientId = "cff76213089a4e228206c8bf120d67cb";
-const redirectUri = "http://localhost:3000/";
+const clientId = "5c822f08eea54e759bac0247ced7b906";
+const redirectUri = "https://3000-ntrance-spotifymetadata-uq962vv9qkj.ws-us27.gitpod.io/";
 const scopes = [
   "user-read-currently-playing",
   "user-read-recently-played",
@@ -26,3 +26,8 @@ export const getTokenFromResponse = () => {
 export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
   "%20"
 )}&response_type=token&show_dialog=true`;
+
+
+// export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+//   "%20"
+// )}&response_type=BQDQ7rfhbhfpJESEsSY5OZlVXUoT-QlF_UN74qG2MIghRLfYC-4oY_m1jv7F5W5siTE_fMKM4rXUYuVLixwaGCk2T6XNRJurg_sVHHO3bRgeP8t5RvubYmyyHMDDnZRpkPF-3umq1DhGyQBdFG8KZFFl086TjQEde-c&show_dialog=true`;
